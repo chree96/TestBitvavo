7e5bbcb5f2ff9a139e393ba1ca15af0cc3d0d062
silviawolves <silviacanepa.w@gmail.com>
2025-04-10 20:37:26 +0200

    updated home

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,15 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { FC } from 'react';
-import { Text, View } from 'react-native';
-import styles from './Home.styles';
+import React, { memo, useCallback, useEffect } from "react";
+import { View, Text, Button } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { wsDisconnect, wsSubscribe } from "../../redux/slices/wsSlice";
+import { Screens } from "../../navigator/StackNavigator.types";
+import useNavigation from "../../navigator/useNavigation";
+import { useFocusEffect } from "@react-navigation/native";
 
-const Home: FC = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+const Home: React.FC = memo(() => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const { priceUpdates, isConnected, subscribedAsset } = useSelector(
+    (state: RootState) => state.websocket
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log("subscribe asset");
+      dispatch(wsSubscribe("btcusdt"));
+
+      return () => {
+        dispatch(wsDisconnect());
+      };
+    }, [])
+  );
+
+  return (
+    <View>
+      <Text>
+        Stato connessione: {isConnected ? "✅ Connesso" : "❌ Disconnesso"}
+      </Text>
+
+      <Text>
+        Prezzo {subscribedAsset}: ${priceUpdates || "--"}
+      </Text>
+
+      <Button
+        title="Go to detail"
+        onPress={() => navigation.navigate(Screens.DETAILS)}
+      />
+    </View>
+  );
+});
 
 export default Home;
